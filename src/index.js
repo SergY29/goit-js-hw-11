ec8b384c2c4ae9754457efde0f2c16ebf0e9c259
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-11-11 14:42:10 +0200

    2

--- src/index.js
+++ src/index.js
@@ -4,24 +4,30 @@ import Notiflix from 'notiflix';
 import { fetchPictures } from "./fetchPictures";
 
 const refs = {
-    form: document.querySelector('#search-form'),
-    gallery: document.querySelector('.gallery'),
+  form: document.querySelector('#search-form'),
+  divGallery: document.querySelector('.gallery'),
 };
 
 
 
+
+
+
+
 fetchPictures('red').then(response => {
-    if (response.length === 0) {
-        // console.log(response);
-        Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-        return
-    } else {
-        // const value = response.data.hits;
-        const markup = response.map((value) => {
-            const { webformatURL, tags } = value
-            return `<div class="photo-card">
+  if (response.length === 0) {
+    // console.log(response);
+    Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+    return
+  } else {
+    // const value = response.data.hits;
+    const markup = response.map((value) => {
+      const { largeImageURL, webformatURL, tags } = value
+      return `<div class="photo-card">
+        <a href="${largeImageURL}">
       <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      <div class="info">
+      </a>
+        <div class="info">
         <p class="info-item">
           <b>Likes</b>
         </p>
@@ -35,12 +41,16 @@ fetchPictures('red').then(response => {
           <b>Downloads</b>
         </p>
       </div>
+      
     </div>`})
-            .join("");
-        refs.gallery.innerHTML = markup;
-        // console.log(markup)
-    }
-
+      .join("");
+    refs.divGallery.innerHTML = markup;
+    // console.log(markup)
+  }
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+  });
 
 });
 
